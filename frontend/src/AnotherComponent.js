fde180393149f76cf294321f586c576c6f4bce2f
ugonna <ugonnathelma@gmail.com>
2018-10-24 12:25:29 +0100

    add frontend

---
+++ frontend/src/AnotherComponent.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <button type="button">Hello</button>;
+  }
+}
+
+export default App;
